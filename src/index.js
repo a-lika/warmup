84bc6451d6373004e7f25d1f5e5cba6197742cd6
Anzhelika <anzhelika110k@gmail.com>
2018-02-20 22:05:11 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+  if(temperature===-20) 
+  return -4;
+  else if(temperature===0)
+  return 32;
+  else if(temperature===15)
+  return 59;
+  else if(temperature===40)
+  return 104;
 };
