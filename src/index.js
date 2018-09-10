a33e7e5c74c4425dc507ab8b0a0153ebba13c1c1
Anzhelika <anzhelika110k@gmail.com>
2018-09-10 21:47:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
-  var celc;
-  if(temperature===-20) 
-  celc = -4;
-  else if(temperature===0)
-  celc = 32;
-  else if(temperature===15)
-  celc = 59;
-  else if(temperature===40)
-  celc = 104;
-  return celc;
+    const fahrenheit = temperature * (9/5) + 32;
+    return fahrenheit;
 };
